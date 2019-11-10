fe415f8fd805cc6d8affc7e06e3c3c279f51568c
Leon Seipp <l.seipp@microeinhundert.com>
2019-11-10 15:01:10 +0100

    Added build script

---
+++ dist/index.js
@@ -0,0 +1,240 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var HTMLDocumentUpdater = function () {
+  _createClass(HTMLDocumentUpdater, [{
+    key: "updateAttributes",
+    value: function updateAttributes(elementInHtmlDocument, elementInUpdatedHtmlDocument, attributes, updateIdentifier) {
+      var _this = this;
+
+      attributes.forEach(function (attribute) {
+        if (elementInHtmlDocument && elementInUpdatedHtmlDocument) {
+          if (elementInUpdatedHtmlDocument.hasAttribute(attribute)) {
+            elementInHtmlDocument.setAttribute(attribute, elementInUpdatedHtmlDocument.getAttribute(attribute));
+          } else {
+            elementInHtmlDocument.removeAttribute(attribute);
+          }
+        } else if (_this.options.strictChecking) {
+          throw new Error("The element with update identifier \"".concat(updateIdentifier, "\" does not exist in both HTMLDocuments!"));
+        }
+      });
+    }
+  }, {
+    key: "updateInnerHtml",
+    value: function updateInnerHtml(elementInHtmlDocument, elementInUpdatedHtmlDocument, updateIdentifier) {
+      if (elementInHtmlDocument && elementInUpdatedHtmlDocument) {
+        elementInHtmlDocument.innerHTML = elementInUpdatedHtmlDocument.innerHTML;
+      } else if (this.options.strictChecking) {
+        throw new Error("The element with update identifier \"".concat(updateIdentifier, "\" does not exist in both HTMLDocuments!"));
+      }
+    }
+  }, {
+    key: "formatUpdateIdentifier",
+    value: function formatUpdateIdentifier(identifier) {
+      var $formattedIdentifier = identifier.replace(/-([a-z])/g, function (g) {
+        return g[1].toUpperCase();
+      });
+      return $formattedIdentifier;
+    }
+  }, {
+    key: "handleUpdates",
+    value: function handleUpdates() {
+      var _this2 = this;
+
+      this.elementsInHtmlDocument.forEach(function (element, updateIdentifier) {
+        if (updateIdentifier in _this2.updates) {
+          var $elementInHtmlDocument = element;
+
+          var $elementInUpdatedHtmlDocument = _this2.elementsInUpdatedHtmlDocument.get(updateIdentifier);
+
+          var $elementConfig = _this2.updates[updateIdentifier];
+
+          if ('attributes' in $elementConfig && $elementConfig.attributes.length) {
+            _this2.updateAttributes($elementInHtmlDocument, $elementInUpdatedHtmlDocument, $elementConfig.attributes, updateIdentifier);
+          }
+
+          if ('innerHTML' in $elementConfig && $elementConfig.innerHTML) {
+            _this2.updateInnerHtml($elementInHtmlDocument, $elementInUpdatedHtmlDocument, updateIdentifier);
+          }
+        } else if (_this2.options.strictChecking) {
+          throw new Error("No update configuration found for element with identifier \"".concat(updateIdentifier, "\"!"));
+        }
+      });
+    }
+  }, {
+    key: "collectElements",
+    value: function collectElements() {
+      var $updateIdentifierAttributeSelector = "[".concat(this.options.attributes.updateIdentifier, "]");
+
+      if (this.htmlDocument instanceof HTMLDocument) {
+        this.elementsInHtmlDocument = this.htmlDocument.querySelectorAll($updateIdentifierAttributeSelector);
+      } else {
+        throw new Error('You have to set "htmlDocument" first before updating!');
+      }
+
+      if (this.updatedHtmlDocument instanceof HTMLDocument) {
+        this.elementsInUpdatedHtmlDocument = this.updatedHtmlDocument.querySelectorAll($updateIdentifierAttributeSelector);
+      } else {
+        throw new Error('You have to set "updatedHtmlDocument" first before updating!');
+      }
+    }
+  }, {
+    key: "update",
+    value: function update() {
+      this.collectElements();
+      this.handleUpdates();
+    }
+  }, {
+    key: "options",
+    get: function get() {
+      return this._options;
+    },
+    set: function set(options) {
+      this._options = options;
+    }
+  }, {
+    key: "htmlDocument",
+    get: function get() {
+      return this._htmlDocument;
+    },
+    set: function set(doc) {
+      if (doc instanceof HTMLDocument) {
+        this._htmlDocument = doc;
+      } else if (typeof doc === 'string') {
+        this._htmlDocument = this._domParser.parseFromString(doc, 'text/html');
+      } else {
+        throw new Error('The HTMLDocument must be of type "string" or "HTMLDocument"!');
+      }
+    }
+  }, {
+    key: "updatedHtmlDocument",
+    get: function get() {
+      return this._updatedHtmlDocument;
+    },
+    set: function set(doc) {
+      if (doc instanceof HTMLDocument) {
+        this._updatedHtmlDocument = doc;
+      } else if (typeof doc === 'string') {
+        this._updatedHtmlDocument = this._domParser.parseFromString(doc, 'text/html');
+      } else {
+        throw new Error('The updated (remote) HTMLDocument must be of type "string" or "HTMLDocument"!');
+      }
+    }
+  }, {
+    key: "elementsInHtmlDocument",
+    get: function get() {
+      return this._elementsInHtmlDocument;
+    },
+    set: function set(elements) {
+      var _this3 = this;
+
+      this._elementsInHtmlDocument.clear();
+
+      elements.forEach(function (element) {
+        var $updateIdentifier = _this3.formatUpdateIdentifier(element.getAttribute(_this3.options.attributes.updateIdentifier));
+
+        if ($updateIdentifier) _this3._elementsInHtmlDocument.set($updateIdentifier, element);
+      });
+    }
+  }, {
+    key: "elementsInUpdatedHtmlDocument",
+    get: function get() {
+      return this._elementsInUpdatedHtmlDocument;
+    },
+    set: function set(elements) {
+      var _this4 = this;
+
+      this._elementsInUpdatedHtmlDocument.clear();
+
+      elements.forEach(function (element) {
+        var $updateIdentifier = _this4.formatUpdateIdentifier(element.getAttribute(_this4.options.attributes.updateIdentifier));
+
+        if ($updateIdentifier) _this4._elementsInUpdatedHtmlDocument.set($updateIdentifier, element);
+      });
+    }
+  }, {
+    key: "updates",
+    get: function get() {
+      return this._updates;
+    },
+    set: function set(updates) {
+      this._updates = updates;
+    }
+  }]);
+
+  function HTMLDocumentUpdater() {
+    var config = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+    _classCallCheck(this, HTMLDocumentUpdater);
+
+    _defineProperty(this, "_options", {
+      strictChecking: false,
+      attributes: {
+        updateIdentifier: 'data-update-id'
+      }
+    });
+
+    _defineProperty(this, "_domParser", new DOMParser());
+
+    _defineProperty(this, "_htmlDocument", document);
+
+    _defineProperty(this, "_updatedHtmlDocument", null);
+
+    _defineProperty(this, "_elementsInHtmlDocument", new Map());
+
+    _defineProperty(this, "_elementsInUpdatedHtmlDocument", new Map());
+
+    _defineProperty(this, "_updates", {});
+
+    if ('options' in config) {
+      if (_typeof(config.options) === 'object') {
+        this.options = Object.assign(this.options, config.options);
+      } else {
+        throw new Error('No valid "options" object passed to HTMLDocumentUpdater!');
+      }
+    }
+
+    if ('htmlDocument' in config) {
+      if (typeof config.htmlDocument === 'string' || config.htmlDocument instanceof HTMLDocument) {
+        this.htmlDocument = config.htmlDocument;
+      } else {
+        throw new Error('No valid "htmlDocument" passed to HTMLDocumentUpdater! It has to to be either of type "string" or "HTMLDocument.');
+      }
+    }
+
+    if ('updatedHtmlDocument' in config) {
+      if (typeof config.updatedHtmlDocument === 'string' || config.updatedHtmlDocument instanceof HTMLDocument) {
+        this.updatedHtmlDocument = config.updatedHtmlDocument;
+      } else {
+        throw new Error('No valid "updatedHtmlDocument" passed to HTMLDocumentUpdater! It has to to be either of type "string" or "HTMLDocument.');
+      }
+    }
+
+    if ('updates' in config) {
+      if (_typeof(config.updates) === 'object') {
+        this.updates = config.updates;
+      } else {
+        throw new Error('No valid "updates" object passed to HTMLDocumentUpdater!');
+      }
+    }
+  }
+
+  return HTMLDocumentUpdater;
+}();
+
+var _default = HTMLDocumentUpdater;
+exports["default"] = _default;
